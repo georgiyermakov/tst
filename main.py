8a850832fb09cff4e6a7fd168668a3d7cf88ec25
georgiyermakov <30489427+georgiyermakov@users.noreply.github.com>
2017-07-28 21:27:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import sqlite3
 import telebot
 from telebot import types
- 
-TOKEN = '403760552:AAH6R0EAKTYAvjsOX1noscBStd7XG1WSnw0'
+
+conn = sqlite3.connect('example.db')
+
+TOKEN = '419956455:AAGvt0ESW5g7WmHTT9av557_Tm13rjmXCnM'
 bot = telebot.TeleBot(TOKEN)
- 
+
+@bot.message_handler(regexp="[@]")
+def handle_message(message):
+        bot.send_message(chat_id=message.chat.id, text="Спасибо! Мы получили ваш e-mail!\n\n/start — вернуться в основное меню")
+        bot.send_message(chat_id='401872534', text=message)
+
+
+
 @bot.message_handler(commands=['start'])
 def start(m):
     """Отвечаем на команду /start
@@ -12,31 +22,20 @@ def start(m):
     keyboard.row(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['СДЕЛАТЬ КРУТЫЕ БРЭЙДЫ']])
     keyboard.row(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['СФОТАТЬСЯ В ЗЕЛЕНОМ АВТОБУСЕ']])
     keyboard.row(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['ПОЛУЧИТЬ СЕКРЕТНУЮ СКИДКУ']])
-    keyboard.row(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['СОХРАНИТЬ МОДНЫЙ СТИКЕРПАК']])
     keyboard.row(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['СТАТЬ АМБАССАДОРОМ']])
     bot.send_message(m.chat.id, "Привет, я чат-бот MATRIX, который будет сегодня жужжать пчелой для всех гостей ПИКНИКА АФИШИ! Давай расскажу, что мы приготовили для тебя сегодня:", reply_markup=keyboard)
- 
+
 @bot.callback_query_handler(func= lambda c: True)
 def inline(c):
     if c.data == 'СДЕЛАТЬ КРУТЫЕ БРЭЙДЫ':
-        bot.send_message(chat_id=c.message.chat.id, text="Какой фестиваль можно представить без кос? Команда стилистов MATRIX целый день будет плести яркие брэйды с канекалоном всем желающим абсолютно бесплатно! Для того чтобы записаться, подойди на стойку ресепшн в бьюти-баре MATRIX. Торопись, желающих очень много!")
-    elif c.data == 'СФОТАТЬСЯ В ЗЕЛЕНОМ АВТОБУСЕ':
-        bot.send_message(chat_id=c.message.chat.id, text="В нашем зеленом фотобасе можно сделать крутые фото, которые сразу можно отправить себе на почту или распечатать прямо на месте. А если выложить фото в соцсети с #liveRAW и показать нашему промоутеру, то можно получить набор крутых наклеек BIOLAGE RAW. Торопись, количество наклеек ограничено")
-    elif c.data == 'ПОЛУЧИТЬ СЕКРЕТНУЮ СКИДКУ':
-        bot.send_message(chat_id=c.message.chat.id, text="Крутые продукты профессионального ухода за волосами MATRIX со скидкой 30% на www.matrix.ru. До конца лета для всех гостей ПИКНИКА по кодовому слову PICNICMATRIX. Ура!")
-    elif c.data == 'СОХРАНИТЬ МОДНЫЙ СТИКЕРПАК':
-        bot.send_message(chat_id=c.message.chat.id, text="Лови набор стикеров!")
-    elif c.data == 'СТАТЬ АМБАССАДОРОМ':
-        bot.send_message(chat_id=c.message.chat.id, text='Этой осенью запустится профессиональная эко-гамма по уходу за волосами Biolage RAW. Это не просто продукты, это манифест иного образа жизни. Хочешь узнать первым и стать лидером мнений новой гаммы? Оставляй свой e-mail, и мы отправим тебе инструкцию. /ttttt')
-        
-            
-@bot.message_handler(commands=['ttttt'])
-def ttttt(message):
-    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
-    bot.register_next_step_handler(sent, hello)
+        bot.send_message(chat_id=c.message.chat.id, text="Какой фестиваль можно представить без кос? Команда стилистов MATRIX целый день будет плести яркие брэйды с канекалоном всем желающим абсолютно бесплатно! Для того чтобы записаться, подойди на стойку ресепшн в бьюти-баре MATRIX. Торопись, желающих очень много!\n\n/start — вернуться в основное меню")
        
-def hello(message):
-    bot.send_message(message.chat.id, 'Привет, {name}. dsafdsafadsf.'.format(name=message.text))
-    bot.send_message(hat_id='178036573', text='[name]') 
- 
+    elif c.data == 'СФОТАТЬСЯ В ЗЕЛЕНОМ АВТОБУСЕ':
+        bot.send_message(chat_id=c.message.chat.id, text="В нашем зеленом фотобасе можно сделать крутые фото, которые сразу можно отправить себе на почту или распечатать прямо на месте. А если выложить фото в соцсети с #liveRAW и показать нашему промоутеру, то можно получить набор крутых наклеек BIOLAGE RAW. Торопись, количество наклеек ограничено\n\n/start — вернуться в основное меню")
+    elif c.data == 'ПОЛУЧИТЬ СЕКРЕТНУЮ СКИДКУ':
+        bot.send_message(chat_id=c.message.chat.id, text="Крутые продукты профессионального ухода за волосами MATRIX со скидкой 30% на www.matrix.ru. До конца лета для всех гостей ПИКНИКА по кодовому слову PICNICMATRIX. Ура!\n\n/start — вернуться в основное меню")
+    elif c.data == 'СТАТЬ АМБАССАДОРОМ':
+        bot.send_message(chat_id=c.message.chat.id, text="Этой осенью запустится профессиональная эко-гамма по уходу за волосами Biolage RAW. Это не просто продукты, это манифест иного образа жизни. Хочешь узнать первым и стать лидером мнений новой гаммы? Оставляй свой e-mail, и мы отправим тебе инструкцию.\nПожалуйста, введите ваш e-mail:")
+
+
 bot.polling()
